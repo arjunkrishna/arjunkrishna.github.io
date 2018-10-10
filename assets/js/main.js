f325a0eb5cbbd898aa1928989fd40d56854b0f70
Arjun Krishna <arjunkrishna@gmail.com>
2018-10-10 16:54:57 -0400

    removed scrolling as it was broken in chrome61.

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,8 +8,15 @@
 		/*----------------------------------*/
 		
 		// init scroll-to effect navigation, adjust the scroll speed in milliseconds
-		$('#main-nav').localScroll(1000);
-		$('#header').localScroll(1000);
+		$('#main-nav a, #header a').on('click', function(e) {
+			e.preventDefault();
+
+			var target = $($(this).attr('href'));
+
+			$('html, body').animate({
+				scrollTop: target.offset().top
+			}, 1000);
+		});
 
 
 		/*-----------------------------------/
